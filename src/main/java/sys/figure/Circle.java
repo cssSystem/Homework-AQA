fdfd4c0353bc0032e8163451d14c449675ea8423
system slike <system-slike@yandex.ru>
2025-11-14 23:22:11 +0300

    lesson_2.3

--- src/main/java/sys/figure/Circle.java
+++
@@ -1,9 +0,0 @@
-package sys.figure;
-
-import sys.figure.ext.FigureEx;
-
-public class Circle extends FigureEx {
-    public Circle(float r, String color, String colorBorder) {
-        super(new float[]{r}, color, colorBorder);
-    }
-}
