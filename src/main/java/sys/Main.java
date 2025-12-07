4a852098477a597666b498e1c965bab05c965baa
system slike <system-slike@yandex.ru>
2025-12-07 21:16:29 +0300

    delete comment main

--- src/main/java/sys/Main.java
+++ src/main/java/sys/Main.java
@@ -4,16 +4,16 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-//        Product[] product = {
-//                new Product("Samsung S25 Ultra", "01.02.2025", "Samsung Corp.", "Korea", 5599, true),
-//                new Product("Samsung S26 Ultra", "01.02.2025", "Samsung Corp.", "Korea", 7599, true),
-//                new Product("Samsung S27 Ultra", "01.02.2025", "Samsung Corp.", "Korea", 9599, true),
-//                new Product("Samsung S28 Ultra", "01.02.2025", "Samsung Corp.", "Korea", 15599, false),
-//                new Product("Samsung S29 Ultra", "01.02.2025", "Samsung Corp.", "Korea", 25599, true)
-//        };
-//        for (Product p : product) {
-//            p.printConsole();
-//        }
+        Product[] product = {
+                new Product("Samsung S25 Ultra", "01.02.2025", "Samsung Corp.", "Korea", 5599, true),
+                new Product("Samsung S26 Ultra", "01.02.2025", "Samsung Corp.", "Korea", 7599, true),
+                new Product("Samsung S27 Ultra", "01.02.2025", "Samsung Corp.", "Korea", 9599, true),
+                new Product("Samsung S28 Ultra", "01.02.2025", "Samsung Corp.", "Korea", 15599, false),
+                new Product("Samsung S29 Ultra", "01.02.2025", "Samsung Corp.", "Korea", 25599, true)
+        };
+        for (Product p : product) {
+            p.printConsole();
+        }
         Park park = new Park();
         park.setAttractions(new Park.Attraction("Колесо обозрения", "14:50", 500));
         park.setAttractions(new Park.Attraction("Паровозики", "10:30", 500));
