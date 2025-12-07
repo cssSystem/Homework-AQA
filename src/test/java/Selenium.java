87f93c77f20eefc17258690c94f07ef7b8169c05
system slike <system-slike@yandex.ru>
2025-12-07 21:14:10 +0300

    delete import is null

--- src/test/java/Selenium.java
+++ src/test/java/Selenium.java
@@ -1,7 +1,6 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
-import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
